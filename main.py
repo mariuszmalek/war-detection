8807e6d66261daf60296e065d6228c7a8f6c6723
Mariusz <mariuszmalek.dev@gmail.com>
2022-10-18 21:09:24 +0200

    clear code

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ import schedule
 import time
 import core
 
-def job():
-    print("I'm watching planes...")
-    core.watch()
+# def job():
+#     print("I'm watching planes...")
+#     core.watch()
  
-schedule.every(1).minutes.do(job)
+# schedule.every(1).minutes.do(job)
     
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
+
+
+print("I'm watching planes...")
+core.watch()
