bd94c2c9ebb75495a48490a046d960d6d7c5fa2e
Mariusz <mariuszmalek.dev@gmail.com>
2022-10-18 07:37:13 +0200

    path to creds

--- clients/google.py
+++ clients/google.py
@@ -8,17 +8,11 @@ load_dotenv()
 class GoogleSpreadsheet():
     def post(self):
         
-        creds = None
-        
-        if os.path.exists('./config/war-detection-2927522c3758.json'):
-            creds = Credentials.from_authorized_user_file('./config/war-detection-2927522c3758.json', SCOPES)
-         
-        if creds:   
-            sa = gspread.service_account()
-            sh = sa.open("War detection")
+        sa = gspread.service_account(filename='./config/war-detection-2927522c3758.json')
+        sh = sa.open("War detection")
 
-            wks = sh.worksheet("Planes data")
-            # records = wks.get_all_records()
-            # values = wks.get_all_values()
-            
-            wks.update('A3', 'Anthony')
+        wks = sh.worksheet("Planes data")
+        # records = wks.get_all_records()
+        # values = wks.get_all_values()
+        
+        wks.update('A3', 'Anthony')
